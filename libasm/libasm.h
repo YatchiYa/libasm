99dbb37a108973cee679a4b77382720491105951
Youcef ARAB <yarab@e2r12p13.42.fr>
2020-02-28 19:34:01 +0100

    sa

--- libasm/libasm.h
+++ libasm/libasm.h
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libasm.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yarab <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/28 19:31:13 by yarab             #+#    #+#             */
+/*   Updated: 2020/02/28 19:32:53 by yarab            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBASM_H
 # define LIBASM_H
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
 
 size_t		ft_strlen(const char *str);
-char		*ft_strcpy(char * dst, const char * src);
+char		*ft_strcpy(char *dst, const char *src);
 int			ft_strcmp(const char *s1, const char *s2);
 ssize_t		ft_write(int fildes, const void *buf, size_t nbyte);
 ssize_t		ft_read(int fildes, void *buf, size_t nbyte);
-char *		ft_strdup(const char *s1);
+char		*ft_strdup(const char *s1);
 
 #endif
